f1cc1b9db90a8519afbdcf887035beb00a47edbb
keresztpapa <48327504+keresztpapa@users.noreply.github.com>
2021-03-16 14:10:25 +0100

    Add files via upload

--- notes/src/com/company/Notes.java
+++ notes/src/com/company/Notes.java
@@ -1,36 +1,44 @@
 package com.company;
+import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 import java.util.ArrayList;
 
-import static java.lang.Thread.sleep;
 
 class Main extends JFrame implements ActionListener {
     static JTextField text;
     static JFrame frame;
     static JButton button;
     static JLabel label;
+
+
+    JLabel labels[];
     ArrayList<String> notes = new ArrayList<String>();
 
     public static void main(String[] args)
     {
         frame = new JFrame("notes");
+        frame.setLayout(new FlowLayout());
         label = new JLabel("nothing entered");
         button = new JButton("submit");
 
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
         Main main = new Main();
+
+        JPanel p = new JPanel();
+
         button.addActionListener(main);
 
         text = new JTextField(16);
 
-        JPanel p = new JPanel();
-        p.add(text);
-        p.add(button);
-        p.add(label);
+
+            p.add(text);
+            p.add(button);
+            p.add(label);
 
         frame.add(p);
-        frame.setSize(300, 300);
+        frame.setSize(400, 400);
         frame.setVisible(true);
     }
 
@@ -39,11 +47,16 @@ class Main extends JFrame implements ActionListener {
         String s = e.getActionCommand();
         if (s.equals("submit")) {
             notes.add(text.getText());
+            JLabel labels[] = new JLabel[notes.size()];
 
             for (int i = 0; i<notes.size(); i++){
-                label.setText(notes.get(i));
-                System.out.println(notes.get(i));
+
+                labels[i] = new JLabel(notes.get(i));
+                labels[i].setText(notes.get(i));
+                frame.add(labels[i]);
+                //System.out.println(notes.get(i));
             }
+
             //label.setText(text.getText());
             text.setText("  ");
         }
