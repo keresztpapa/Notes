f0e98322b74dcd1e29a662ac96ac18e37641508d
keresztpapa <48327504+keresztpapa@users.noreply.github.com>
2021-03-16 13:22:54 +0100

    Add files via upload

--- notes/src/com/company/Notes.java
+++ notes/src/com/company/Notes.java
@@ -1,25 +1,34 @@
 package com.company;
 import java.awt.event.*;
 import javax.swing.*;
+import java.util.ArrayList;
+
+import static java.lang.Thread.sleep;
 
 class Main extends JFrame implements ActionListener {
     static JTextField text;
     static JFrame frame;
     static JButton button;
     static JLabel label;
+    ArrayList<String> notes = new ArrayList<String>();
 
     public static void main(String[] args)
     {
         frame = new JFrame("notes");
         label = new JLabel("nothing entered");
         button = new JButton("submit");
-        Main no = new Main();
-        button.addActionListener(no);
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        Main main = new Main();
+        button.addActionListener(main);
+
         text = new JTextField(16);
+
         JPanel p = new JPanel();
         p.add(text);
         p.add(button);
         p.add(label);
+
         frame.add(p);
         frame.setSize(300, 300);
         frame.setVisible(true);
@@ -29,7 +38,13 @@ class Main extends JFrame implements ActionListener {
     {
         String s = e.getActionCommand();
         if (s.equals("submit")) {
-            label.setText(text.getText());
+            notes.add(text.getText());
+
+            for (int i = 0; i<notes.size(); i++){
+                label.setText(notes.get(i));
+                System.out.println(notes.get(i));
+            }
+            //label.setText(text.getText());
             text.setText("  ");
         }
     }
